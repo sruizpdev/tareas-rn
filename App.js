9264655becdb4bd04987dc27206dd071275caef6
sergiotto <sergiotto@outlook.com>
2023-03-06 11:57:29 +0100

    primera prueba con AS funcionando

--- App.js
+++ App.js
@@ -3,13 +3,34 @@ import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Form from './src/components/Form';
 import Tasks from './src/components/Tasks';
 import {mokData} from './src/mokData';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
   const [task, setTask] = useState({});
   const [tasks, setTasks] = useState([]);
-  useEffect(() => {
 
-    
+  useEffect(() => {
+    const saveData = async () => {
+      const jsonValue = JSON.stringify(mokData);
+      try {
+        await AsyncStorage.setItem('key', jsonValue);
+      } catch (e) {
+        console.log('error al guardar');
+      }
+    };
+
+    const getData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem('key');
+
+        console.log(JSON.parse(jsonValue));
+      } catch (e) {
+        console.log('error al sacar el dato de AS');
+      }
+    };
+    saveData();
+    getData();
+
     setTasks(mokData);
   }, []);
 
